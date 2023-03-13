0a479d57eb616273fcc0adba2e77261019cc71ae
Josh Fester <joshfester@gmail.com>
2023-03-13 05:13:32 +0000

    Allow global search for users in avo

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ gem "avo", "~> 2.27.1"
 gem "devise", "~> 4.9.0"
 gem "oj", "~> 3.14.2"
 gem "pundit", "~> 2.3.0"
+gem 'ransack', "~> 4.0.0"
 gem "view_component", "~> 2.82.0"
 
 group :development, :test do
--- app/avo/resources/user_resource.rb
+++ app/avo/resources/user_resource.rb
@@ -1,12 +1,17 @@
 class UserResource < Avo::BaseResource
-  self.title = :id
+  self.title = :email
   self.includes = []
   # self.search_query = -> do
   #   scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
   # end
+  
+  self.search_query = -> do
+    scope.ransack(id_eq: params[:q], email_cont: params[:q], m: "or").result(distinct: false)
+  end
 
   field :id, as: :id
   # Fields generated from the model
   field :email, as: :text
   # add fields here
+  
 end
