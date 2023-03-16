74968ff757b529147d3524150d2ff542bf07375a
Josh Fester <joshfester@gmail.com>
2023-03-16 04:13:30 +0000

    add role to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable
 
+  enum role: {
+    reader: 0,
+    editor: 1,
+    admin: 2
+  }
+
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "email", "id", "remember_created_at", "reset_password_sent_at", "updated_at"]
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_14_045247) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_16_040915) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -82,6 +82,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_14_045247) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
